7f8b2b33cfa204c5681a7d0f575b2ab7308740da
Nicoco220983 <nicolas.carrez@gmail.com>
2026-03-12 16:15:08 +0100

    Fix local IP

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,12 +9,14 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    host: true,
     proxy: {
       '/ws': { target: 'ws://localhost:8080', ws: true, rewrite: (path) => path.replace(/^\/ws/, '') },
     },
   },
   preview: {
     port: 3000,
+    host: true,
     proxy: {
       '/ws': { target: 'ws://localhost:8080', ws: true, rewrite: (path) => path.replace(/^\/ws/, '') },
     },
