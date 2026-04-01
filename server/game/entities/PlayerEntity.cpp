c5e4edde621ac5b64a7540e54ea5f1afc0ef31f5
Nicoco220983 <nicolas.carrez@gmail.com>
2026-04-01 23:28:00 +0200

    remove code duplication

--- server/game/entities/PlayerEntity.cpp
+++ server/game/entities/PlayerEntity.cpp
@@ -1,36 +1,13 @@
 #include "game/entities/PlayerEntity.hpp"
 #include "game/entities/EntityFactory.hpp"
-#include "game/components/DynamicPositionComponent.hpp"
-#include "game/components/EntityTypeComponent.hpp"
-#include "game/components/InputComponent.hpp"
-#include "game/components/PlayerComponent.hpp"
-#include "game/components/BoundingBoxComponent.hpp"
-#include "game/components/PhysicsModeComponent.hpp"
 
 namespace voxelmmo::PlayerEntity {
-
-namespace {
-    constexpr int32_t PLAYER_BBOX_HX = 128;  // 0.5 voxels
-    constexpr int32_t PLAYER_BBOX_HY = 256;  // 1 voxel  
-    constexpr int32_t PLAYER_BBOX_HZ = 128;  // 0.5 voxels
-}
 
 entt::entity spawnImpl(entt::registry& reg,
                        GlobalEntityId globalId,
                        const EntitySpawnRequest& req)
 {
-    // Base components (GlobalEntityId, Dirty, ChunkMembership, PendingCreate)
-    const entt::entity ent = BaseEntity::spawn(reg, globalId, req.x, req.y, req.z);
-
-    // Player-specific components
-    reg.emplace<DynamicPositionComponent>(ent, req.x, req.y, req.z, 0, 0, 0, /*grounded=*/false);
-    reg.emplace<EntityTypeComponent>(ent, EntityType::PLAYER);
-    reg.emplace<InputComponent>(ent);
-    reg.emplace<PlayerComponent>(ent, req.playerId);
-    reg.emplace<BoundingBoxComponent>(ent, PLAYER_BBOX_HX, PLAYER_BBOX_HY, PLAYER_BBOX_HZ);
-    reg.emplace<PhysicsModeComponent>(ent, PhysicsMode::FULL);
-
-    return ent;
+    return spawn(reg, globalId, req.x, req.y, req.z, req.playerId);
 }
 
 } // namespace voxelmmo::PlayerEntity
--- server/game/entities/SheepEntity.cpp
+++ server/game/entities/SheepEntity.cpp
@@ -7,23 +7,7 @@ entt::entity spawnImpl(entt::registry& reg,
                        GlobalEntityId globalId,
                        const EntitySpawnRequest& req)
 {
-    // Base components (GlobalEntityId, Dirty, ChunkMembership, PendingCreate)
-    const entt::entity ent = BaseEntity::spawn(reg, globalId, req.x, req.y, req.z);
-
-    // Sheep-specific components
-    reg.emplace<DynamicPositionComponent>(ent, req.x, req.y, req.z, 0, 0, 0, /*grounded=*/true);
-    reg.emplace<EntityTypeComponent>(ent, EntityType::SHEEP);
-    reg.emplace<BoundingBoxComponent>(ent, SHEEP_BBOX_HX, SHEEP_BBOX_HY, SHEEP_BBOX_HZ);
-    reg.emplace<PhysicsModeComponent>(ent, PhysicsMode::FULL);
-
-    // Start in IDLE state for 2-5 seconds (120-300 ticks at 60tps)
-    uint32_t idleTicks = 120 + (req.startTick % 180);
-    reg.emplace<SheepBehaviorComponent>(ent,
-        SheepBehaviorComponent::State::IDLE,
-        req.startTick + idleTicks,
-        req.x, req.z, 0.0f);
-
-    return ent;
+    return spawn(reg, globalId, req.x, req.y, req.z, req.startTick);
 }
 
 } // namespace voxelmmo::SheepEntity
